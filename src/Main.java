c5b8c181e2809011632001f51e0957f27edefc6d
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-08-09 17:22:07 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int age = 19;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он достиг совершеннолетия.");
+        }
+        int age1 = 17;
+        if (age1 < 18) {
+            System.out.println("Если возраст человека равен " + age1 + " он не достиг совершеннолетия, нужно немного подождать.");
+        }
     }
 }
