76e06ac8583b9182aa0347170f004a4c84906fb4
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-08-09 17:40:45 +0300

    задача 2

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,16 @@ public class Main {
         if (age < 18) {
             System.out.println("Если возраст человека равен " + age + " он не достиг совершеннолетия, нужно немного подождать.");
         }
+
+        int gradus = 10;
+        if (gradus > 5) {
+            System.out.println(" На улице " + gradus + " градусов, сегодня тепло,можно идти без шапки.");
+        }
+        int gradus1 = 4;
+        if (gradus1 < 5) {
+            System.out.println(" На улице " + gradus1 + " градусов, на улице холодно, нужно надеть шапку.");
+        }
+
+
     }
 }
