76d60f6df86329b904fe9cc29a0ec7c08433e2d6
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-08-10 09:38:44 +0300

    исправления

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,22 @@ public class Main {
         int age = 17;
         if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + ", то он достиг совершеннолетия.");
-        }
-        if (age < 18) {
+        } else {
             System.out.println("Если возраст человека равен " + age + " он не достиг совершеннолетия, нужно немного подождать.");
         }
 
         int gradus = 10;
         if (gradus > 5) {
             System.out.println(" На улице " + gradus + " градусов, сегодня тепло,можно идти без шапки.");
-        }
-        int gradus1 = 4;
-        if (gradus1 < 5) {
-            System.out.println(" На улице " + gradus1 + " градусов, на улице холодно, нужно надеть шапку.");
+        } else {
+            System.out.println(" На улице холодно, нужно надеть шапку.");
         }
 
         int speed = 70;
-        int speed1 = 50;
         if (speed > 60) {
             System.out.println("Если скорость " + speed + ", придется заплатить штраф.");
-        }
-        if (speed1 <= 60) {
-            System.out.println("Если скорость " + speed1 + ", можно ездить спокойно.");
+        } else {
+            System.out.println("Можно ездить спокойно.");
         }
 
         int agePerson = 3;
@@ -45,10 +40,10 @@ public class Main {
 
         int ageChaild = 4;
         if (ageChaild < 5) {
-            System.out.println("Если возрат ребенка равен " + ageChaild + ", то ему нельзя кататься на атракицоне.");
+            System.out.println("Если возраст ребенка равен " + ageChaild + ", то ему нельзя кататься на атракицоне.");
         }
         int ageChaild1 = 7;
-        if (ageChaild1 > 5 && ageChaild < 14) {
+        if (ageChaild1 > 5 && ageChaild1 < 14) {
             System.out.println("Если возраст ребенка равен " + ageChaild1 + ", то он может кататься на атракционе в сопровождении взрослого.");
         }
         int ageChaild2 = 15;
@@ -76,18 +71,10 @@ public class Main {
         int three = 3;
         if (one > two && one > three) {
             System.out.println(" Число one является большим.");
-        } else {
-            System.out.println(" Число one не является большим.");
-        }
-        if (two > one && two > three) {
+        } else if (two > one && two > three) {
             System.out.println(" Число two является большим.");
-        } else {
-            System.out.println(" Число two не является большим.");
-        }
-        if (three > one && three > two) {
+        } else if (three > one && three > two) {
             System.out.println(" Число three является большим.");
-        } else {
-            System.out.println(" Число three не является большим.");
         }
     }
 }
